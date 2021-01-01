43140e3b8d9988fa5942cc7849cd20bf4de9699c
unknown <lonecreation1612@gmail.com>
2021-01-01 22:05:59 +0400

    Cleaning code

--- js/templatemo_custom.js
+++ js/templatemo_custom.js
@@ -58,19 +58,3 @@ jQuery(document).ready(function($) {
 
 });
 
-
-function loadScript() {
-	  var script = document.createElement('script');
-	  script.type = 'text/javascript';
-	  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
-	      'callback=initialize';
-	  document.body.appendChild(script);
-	}
-
-	function initialize() {
-	    var mapOptions = {
-	      zoom: 15,
-	      center: new google.maps.LatLng(16.8496189,96.1288854)
-	    };
-	    var map = new google.maps.Map(document.getElementById('map_canvas'),  mapOptions);
-	}
